e9a271026f00dfab0e8fc870067305bfd04966ec
OlegMRCL <helgicannoli@gmail.com>
2018-11-26 22:39:35 +0300

    Rewrite status functions to return (ok bool, err error)

--- status.go
+++ status.go
@@ -10,42 +10,63 @@ import (
 //If there is no "generatorID" key the app will try to become generator;
 //If the value on "generatorID" key is equal to app.id then the app is generator already and status wil be updated;
 //In any other case the app is not a generator.
-func (app *App) checkStatus () {
+func (app *App) checkStatus () (bool, error) {
 	generatorID, err := app.client.Get("generatorID").Result()
-	if err == redis.Nil {
-		app.setStatus()
-	} else if err != nil {
+
+	switch {
+	case err == redis.Nil :
+		return statusFunc(app.setStatus)
+
+	case err != nil :
 		fmt.Println("ERROR occurred while CHECKING the generator status!: ", err)
-	} else if generatorID == app.id {
-		app.updateStatus()
-	} else {
+		return false, err
+
+	case generatorID == app.id :
+		return statusFunc(app.updateStatus)
+
+	default :
 		app.isGenerator = false
+		return false, err
+	}
+}
+
+func statusFunc(f func() (bool, error)) (bool, error) {
+	if ok, err := f(); ok {
+		return true, err
+	} else {
+		return false, err
 	}
 }
 
 //Try to set app.id as generatorID to Redis database only if the "generatorID" key does not exist.
 //If redis cmd is successful then the app has become a generator;
 //If "generatorID" key exists already then the app is not a generator.
-func (app *App) setStatus() {
+func (app *App) setStatus() (bool, error) {
 	reply := app.client.SetNX("generatorID", app.id, time.Millisecond * 1000)
 	if err := reply.Err(); err != nil {
 		fmt.Println("ERROR occurred while STARTING the generator!", err)
 		app.isGenerator = false
+		return false, err
 	} else if reply.Val() == false {
 		fmt.Println("Attempt to start generator FAILED! Generator has been started already")
 		app.isGenerator = false
+		return false, err
 	} else {
 		fmt.Println("Generator was started successfully!")
 		app.isGenerator = true
+		return true, err
 	}
 }
 
 //Update the app status by extending the existence of the "generatorID" key
-func (app *App) updateStatus() {
+func (app *App) updateStatus() (bool, error) {
 	reply := app.client.Expire("generatorID", time.Millisecond * 1000)
 	if err := reply.Err(); err != nil {
 		fmt.Println("ERROR occurred while UPDATING the generator status!")
 		fmt.Println("The generator operation is terminated.")
 		app.isGenerator = false
+		return true, err
+	} else {
+		return true, err
 	}
 }
